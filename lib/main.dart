f34b2f23df114f5b42f8acaa722812cf3f86c1b6
Naor David Melamed <naorcho@gmail.com>
2019-04-17 21:14:46 +0300

    Update the name for iOS and connectivity

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,16 @@
 import 'package:flutter/material.dart';
 import 'package:reut_buy_it_for_me/app.dart';
+import 'package:reut_buy_it_for_me/home_widget.dart';
+import 'package:reut_buy_it_for_me/onboarding/onboarding_controller.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 import 'AppColors.dart';
+import 'utils/connectionStatusSingleton.dart';
 
-void main() => runApp(new MyApp());
+void main() {
+  ConnectionStatusSingleton connectionStatus = ConnectionStatusSingleton.getInstance();
+  connectionStatus.initialize();
+  runApp(new MyApp());
+}
 
 class MyApp extends StatelessWidget {
   // This widget is the root of your application.
@@ -13,8 +21,32 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: AppColors.pink,
       ),
-      home: new App(),
+      home: _getScreen(),
     );
   }
-}
 
+  _getScreen() {
+    int result = 1; //_read();
+    if (result == 1) {
+      return new App();
+    }
+    _save();
+    return OnboardingMainPage();
+  }
+
+  _read() async {
+    final SharedPreferences prefs = await SharedPreferences.getInstance();
+    final key = 'welcome';
+    final value = prefs.getInt(key) ?? 0;
+    print('read: $value');
+    return value;
+  }
+
+  _save() async {
+    final SharedPreferences prefs = await SharedPreferences.getInstance();
+    final key = 'welcome';
+    final value = 1;
+    prefs.setInt(key, value);
+    print('saved $value');
+  }
+}
